1525ca6530889face456a5f1b8a401dcacff7ea1
luckyLukec <luka.lukac@student.um.si>
2022-06-01 17:41:59 +0200

    GUI and functionalities improvements

--- ast_monitor/classes.py
+++ ast_monitor/classes.py
@@ -1,10 +1,11 @@
 class SensorData():
-    def __init__(self, hr, lon, lat, alt, curr_time) -> None:
+    def __init__(self, hr, lon, lat, alt, curr_time, dist) -> None:
         self.hr = hr
         self.lon = lon
         self.lat = lat
         self.alt = alt
         self.curr_time = curr_time
+        self.dist = dist
 
 
 class Interval():
