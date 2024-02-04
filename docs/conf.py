6185f5ac8715fe19dc68634955dcbca2189e67ff
Iztok Fister Jr. <iztok@iztok.xyz>
2024-02-04 09:31:30 +0100

    Bump version to 0.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2022-2023, Iztok Fister Jr. et al.'
 author = 'Iztok Fister Jr., Luka Lukač, Alen Rajšp'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.2'
+release = '0.4.3'
 
 
 # -- General configuration ---------------------------------------------------
