f27719dca27733cbb668bf0fba49ec8a2061f140
Iztok Fister Jr. <iztok@iztok.xyz>
2024-03-25 14:55:34 +0100

    New version - 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022-2023, Iztok Fister Jr. et al.'
 author = 'Iztok Fister Jr., Luka Lukač, Alen Rajšp'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.3'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
