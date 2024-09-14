096546f65ab60f5b8bfc9dcbe4bc6dfac89d1a95
Iztok Fister Jr. <iztok@iztok.dev>
2024-09-14 14:01:41 +0200

    rebuilt and bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022-2024, Iztok Fister Jr. et al.'
 author = 'Iztok Fister Jr., Luka Lukač, Alen Rajšp'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.2'
 
 
 # -- General configuration ---------------------------------------------------
